0285d582e404e257c63c46f2bf3a007e3d6d3c91
Harry Cliff <harryacliff@gmail.com>
2019-07-21 14:15:14 -0400

    Changed parent layout of activity_reading to a ConctraintLayout

--- app/src/main/java/com/woods/blinkreader/activity/ReadingActivity.java
+++ app/src/main/java/com/woods/blinkreader/activity/ReadingActivity.java
@@ -15,17 +15,8 @@ public class ReadingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reading);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        Toolbar toolbar = findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
     }
 
 }
