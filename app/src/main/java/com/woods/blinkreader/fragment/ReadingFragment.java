4cf0be439b3fbdbb619517169da9292702abcec2
Harry Cliff <harryacliff@gmail.com>
2019-07-23 01:07:58 -0400

    Implemented Seekbar for Determining which word in List of words to read

--- app/src/main/java/com/woods/blinkreader/fragment/ReadingFragment.java
+++ app/src/main/java/com/woods/blinkreader/fragment/ReadingFragment.java
@@ -1,7 +1,7 @@
 package com.woods.blinkreader.fragment;
 
-import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -13,36 +13,36 @@ import com.woods.blinkreader.R;
 import com.woods.blinkreader.databinding.FragmentReadingBinding;
 import com.woods.blinkreader.viewmodel.ReadingViewModel;
 
+import java.util.Objects;
+
 import static com.woods.blinkreader.utils.BundleStrings.READING_FRAGMENT_TEXT_KEY;
 
 public class ReadingFragment extends Fragment {
 
-    private String toReadString;
     private ReadingViewModel readingViewModel;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Obtain the ViewModel component.
-
-        View toReturnView = inflater.inflate(R.layout.fragment_reading, container, false);
-        Bundle arguments = getArguments();
+    public void onAttach(Context context) {
+        super.onAttach(context);
         readingViewModel = ViewModelProviders.of(getActivity())
                 .get(ReadingViewModel.class);
-        if(arguments != null) {
-            toReadString = arguments.getString(READING_FRAGMENT_TEXT_KEY);
-            FragmentReadingBinding fragmentReadingBinding = DataBindingUtil.bind(toReturnView);
-            if(fragmentReadingBinding != null) {
-                fragmentReadingBinding.setLifecycleOwner(this);
-                fragmentReadingBinding.setReadingViewModel(readingViewModel);
-                readingViewModel.postText(toReadString);
-            }
+    }
 
-            // Inflate view and obtain an instance of the binding class.
-//         binding = DataBindingUtil.getBinding(toReturnView);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View toReturnView = inflater.inflate(R.layout.fragment_reading, container, false);
 
-            // Specify the current activity as the lifecycle owner.
-//        binding.setLifecycleOwner(this);
+        Bundle arguments = getArguments();
+
+        FragmentReadingBinding fragmentReadingBinding = DataBindingUtil.bind(toReturnView);
+
+        if (arguments != null && arguments.getString(READING_FRAGMENT_TEXT_KEY) != null &&
+                fragmentReadingBinding != null) {
+            fragmentReadingBinding.setLifecycleOwner(this);
+            fragmentReadingBinding.setReadingViewModel(readingViewModel);
+            readingViewModel.postText(Objects.requireNonNull(arguments.getString(READING_FRAGMENT_TEXT_KEY)));
         }
+
         return toReturnView;
     }
 }
--- app/src/main/java/com/woods/blinkreader/viewmodel/ReadingViewModel.java
+++ app/src/main/java/com/woods/blinkreader/viewmodel/ReadingViewModel.java
@@ -3,15 +3,46 @@ package com.woods.blinkreader.viewmodel;
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
+import android.databinding.adapters.SeekBarBindingAdapter;
+import android.support.annotation.NonNull;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ReadingViewModel extends ViewModel {
-    private final MutableLiveData<String> textToDisplayLiveData = new MutableLiveData<>();
 
+    private final MutableLiveData<String> textToDisplayLiveData = new MutableLiveData<>();
+    private final MutableLiveData<Integer> maxProgressLiveData = new MutableLiveData<>();
+    private List<String> wordList;
+
+    @NonNull
     public LiveData<String> getTextToDisplayLiveData() {
         return textToDisplayLiveData;
     }
 
-    public void postText(String toReadString) {
-        textToDisplayLiveData.postValue(toReadString);
+    public MutableLiveData<Integer> getMaxProgressLiveData() {
+        return maxProgressLiveData;
+    }
+
+    public void postText(@NonNull String toReadString) {
+        wordList = new ArrayList<>(Arrays.asList(toReadString.split(" ")));
+        if (!wordList.isEmpty()) {
+            textToDisplayLiveData.postValue(wordList.get(0));
+            maxProgressLiveData.postValue(wordList.size() - 1);
+        }
+    }
+
+    @NonNull
+    public SeekBarBindingAdapter.OnProgressChanged getOnProgressChanged() {
+        return new SeekBarBindingAdapter.OnProgressChanged() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (wordList != null && progress < wordList.size()) {
+                    textToDisplayLiveData.postValue(wordList.get(progress));
+                }
+            }
+        };
     }
 }
